9f08d7324294dae9aa216e16bbf0f466bcc259a8
Thilina dilshan <hbthilinadilshan123@gmail.com>
2020-07-19 19:35:28 +0530

    Update The Caesar cipher.scala

--- The Caesar cipher.scala
+++ The Caesar cipher.scala
@@ -11,8 +11,8 @@ val cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(
 val ct=cipher(E,"THILINA",5,alphabet);
 val pt=cipher(D,ct,5,alphabet);
 
-println(ct);
 println(pt);
+println(ct);
 
 
 }
